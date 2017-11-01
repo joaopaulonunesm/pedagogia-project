f680afbdf88d82a5ff65255b80934b21fc595aae
Joao Marques <joaopaulonunesm@hotmail.com>
2017-11-01 14:45:47 -0200

    Iniciando com Spring Security e apontando banco de dados heroku

---
+++ src/main/java/com/pedagogiaproject/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.pedagogiaproject.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+//			.antMatchers("/admin")
+//				.authenticated()
+			.anyRequest()
+				.permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> inMemoryAuthentication = auth.inMemoryAuthentication();
+			inMemoryAuthentication.withUser("smgedu").password("smgedu").roles("USER");
+	}
+
+}
