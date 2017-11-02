6fa1f18e9d0472b05619792113bdc9e423cd1aca
João Marques <joaopaulonunesm@hotmail.com>
2017-11-02 20:37:45 -0200

    Alterando base Urls

--- src/main/resources/static/js/configs/baseUrl.js
+++ src/main/resources/static/js/configs/baseUrl.js
@@ -1,10 +1,8 @@
 angular.module("pedagogiaApp").value("configs", {
 
-	baseUrl: "http://localhost:8080",
-	siteUrl: "http://localhost:8080",
+	//baseUrl: "http://localhost:8080",
+	//siteUrl: "http://localhost:8080",
 
-	//baseUrl: "https://pedagogiaproject.herokuapp.com",
-	//siteUrl: "https://pedagogiaproject.herokuapp.com",
-
-	version: "/v1"
+	baseUrl: "https://pedagogiaproject.herokuapp.com",
+	siteUrl: "https://pedagogiaproject.herokuapp.com"
 });
